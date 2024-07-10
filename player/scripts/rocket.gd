37c3937006bdc95e4c659047747d5208e196657f
gilmarkandrade <45281863+gilmarkandrade@users.noreply.github.com>
2024-07-10 11:59:20 -0300

    up

--- player/scripts/rocket.gd
+++ player/scripts/rocket.gd
@@ -3,7 +3,9 @@ extends Node2D
 @export var Speed_R1 = 300
 @export var Speed_R2 = 600
 @export var Speed_R3 = 900
-
+@onready var p1 = %particle1
+@onready var p2 = %particle2
+@onready var p3 = $particle3
 
 func _ready():
 	
@@ -15,13 +17,37 @@ func set_type_rocket():
 	match  Global.parameters.rocketLevel:
 		1:
 			anim.play("R1")
+			p1.visible = true
+			p1.emitting = true
+			p2.emitting = false
+			p2.visible = false
+			p3.emitting = false
+			p3.visible = false
 			if player.size() >0:
 				player[0].speed = Speed_R1
+				
 		2:
 			anim.play("R2")
+			p1.visible = false
+			p1.emitting = false
+			p2.emitting = true
+			p2.visible = true
+			p3.emitting = true
+			p3.visible = true
+			p2.position = Vector2(-35,25)
+			p3.position = Vector2(35,25)
 			if player.size() >0:
 				player[0].speed = Speed_R2
 		3:
 			anim.play("R3")
+			p1.visible = true
+			p1.emitting = true
+			p2.emitting = true
+			p2.visible = true
+			p3.emitting = true
+			p3.visible = true
+			p2.position = Vector2(-45,50)
+			p3.position = Vector2(45,50)
 			if player.size() >0:
 				player[0].speed = Speed_R3
+				
